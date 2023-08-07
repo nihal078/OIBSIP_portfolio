2e1d7737b372b54c456e13d7fea64a70464d58b4
Muhammad Nihal <65040107+nihal078@users.noreply.github.com>
2023-08-07 22:24:39 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 
+// mixit up
+
+var mixer = mixitup('.portfolio-gallery');
 
 
 
-
-
-
-
+// active  menu
 
 
 let menuLi = document.querySelectorAll('header ul li a');
@@ -20,13 +20,31 @@ function activeMenu() {
 }
 
 activeMenu();
-window.addEventListener("scroll",activeMenu);
+window.addEventListener("scroll", activeMenu);
 
 
 // sticy nabar
 
 const header = document.querySelectorAll('header');
-window.addEventListener('scroll',function() {
-    header.classList.toggle("sticky",this.window.scrollY > 50)
-    
+window.addEventListener('scroll', function () {
+    header.classList.toggle("sticky", this.window.scrollY > 50)
+
 })
+
+// toggle nabar
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+
+menuIcon.onclick = () => {
+
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+window.onscroll = () => {
+
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove  ("open");
+}
